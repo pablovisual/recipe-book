a7a467f543b424d7f20fc02e46cf86d229e7e317
pabarmsan <pabloarmandosandoval@gmail.com>
2025-07-18 22:20:03 -0500

    add checks to create new recipe.

--- recipe-book-client/app/components/AddRecipe.tsx
+++ recipe-book-client/app/components/AddRecipe.tsx
@@ -1,58 +1,106 @@
 'use client';
 
-import React from 'react';
+import React, { useState} from 'react';
 import { z } from 'zod/v4';
 import { zodResolver } from '@hookform/resolvers/zod';
 import { useForm } from 'react-hook-form';
 import { Button } from "@/components/ui/button";
 import { Textarea } from "@/components/ui/textarea"
 import { Input } from "@/components/ui/input";
-import { Label } from "@/components/ui/label";
 
 interface RecipeData {
-  name: string,
+  title: string,
   time: string,
   ingredients: string,
   description: string,
 }
 
 const recipeSchema = z.object({
-  name: z.string().min(6, { message: 'Must have a name for your dish.'}),
+  title: z.string().min(6, { message: 'Must have a name for your dish.'}),
   time: z.string().min(1, { message: 'Must give a set cook time.'}).max(999, { message: 'Can\'t be that long of cooking time.'}).refine(val => !isNaN(parseInt(val)), { message: 'That\'s not a number.'}),
   ingredients: z.string().min(10, { message: 'Must type out ingredients.'}),
   description: z.string().min(10, { message: 'Must give a description.' }),
 });
 
 const AddRecipe = () => {
+  const [image, setImage] = useState("");
+  const [error, setError] = useState("");
+
   const { register, handleSubmit, formState: { errors } } = useForm<RecipeData>({
     resolver: zodResolver(recipeSchema),
     defaultValues: {
-      name: '',
+      title: '',
       time: '',
       ingredients: '',
-      description: ''
+      description: '',
     },
     mode: "onChange"
   });
-  const createRecipe = (data: RecipeData) => {
-    const { name, time, ingredients, description } = data;
+
+  const getImage = (e:any) => {
+    //grab the image file
+    const img = e.target.files[0];
+
+    //check if file is an image
+    if(!img.name.match(/\.(bpm|jpg|jpeg|png|svg|tif|tiff|webp)$/)) {
+      setError("Wrong file Type!");
+      return;
+    }
+
+    //image can't be greater than 5mb
+    if(img.size > (1024 * 1024 * 5)) {
+      setError(`File size is ${img.size} which is > than 5MB`);
+      return;
+    }
+
+    //make image to base64 string
+    const reader = new FileReader();
+    reader.readAsDataURL(e.target.files[0]);
+
+    reader.onload = () => {
+      console.log(reader.result);
+
+      //set the image with reader.result->string
+      setImage(reader?.result);
+
+    };
+
+    setError("");
+
+    reader.onerror = error => {
+      console.log(`Error: ${error}`);
+    }
+  }
+  const createRecipe = (data:  RecipeData) => {
+    const { title, time, ingredients, description } = data;
+
+    //if error useState is not empty then return nothing
+    if(error.length > 0)
+      return;
+
+    //if user doesn't add image, set error and return nothing
+    if(image.length == 0) {
+      setError('Image file cant be empty');
+      return;
+    }
 
     if(Math.floor(parseInt(time) / 60) == 0) {
       const min:string = (parseInt(time) % 60).toString();
-      console.log(`Title: ${ name } \nCooking time: ${ min } minutes\n Ingredients:\n${ ingredients } \nDescription: ${ description }`);
+      console.log(`Title: ${ title } \nCooking time: ${ min } minutes\n Ingredients:\n${ ingredients } \nDescription: ${ description }`);
     }
 
     else {
       const cookingTime:String = `${ (Math.floor(parseInt(time) / 60)).toString() } hours ${ (parseInt(time) % 60).toString() } minutes`;
-      console.log(`Title: ${ name } \nCooking time: ${ cookingTime } \nIngredients:\n${ ingredients } \nDescription: ${ description }`);
+      console.log(`Title: ${ title } \nCooking time: ${ cookingTime } \nIngredients:\n${ ingredients } \nDescription: ${ description }`);
     }
 
   }
+
   return (
-    <form onSubmit={handleSubmit(createRecipe)} className='p-4 space-y-5'>
+    <form onSubmit={handleSubmit(createRecipe)} className='p-4 space-y-5 h-[80vh] flex flex-col justify-center'>
       <div className="space-y-2 ">
-        <Input className='w-full outline-none' placeholder="Recipe name" {...register('name')} />
-        {errors.name && <p className='text-red-500 text-sm mt-1'>{errors.name.message}</p>}
+        <Input className='w-full outline-none' placeholder="Recipe name" {...register('title')} />
+        {errors.title && <p className='text-red-500 text-sm mt-1'>{errors.title.message}</p>}
       </div>
       <div>
         <Input className='outline-none' type='number' min={1} max={999} placeholder="Cooking Time" {...register('time')} />
@@ -65,6 +113,10 @@ const AddRecipe = () => {
       <div>
         <Textarea placeholder="Type the description on preparing the recipe." {...register('description')} />
         {errors.description && <p className='text-red-500 text-sm mt-1'>{errors.description.message}</p>}
+      </div>
+      <div>
+        <Input id="picture" accept='image/*' onChange={getImage} type="file" />
+        {error && <p className='text-red-500 text-sm mt-1'>{error}</p>}
       </div>
 
       <Button type="submit" variant='outline' className="rounded-full bg-blue-700 text-white text-xl p-4 w-1/4 uppercase primary float-right">
--- recipe-book-client/app/recipes/page.tsx
+++ recipe-book-client/app/recipes/page.tsx
@@ -9,21 +9,21 @@ interface Props {
 
 const Page = () => {
   return (
-    <div className='flex flex-row p-4 h-full'>
-      <div className="w-[30%] mt-44 max-h-screen overflow-y-scroll scrollbar-none">
+    <div className='flex flex-row p-4 h-[87vh]'>
+      <div className="w-[30%]  h-full overflow-y-scroll scrollbar-none">
         <AddRecipe />
       </div>
 
-      <div className="w-[70%] grid grid-cols-3 gap-4 content-evenly md:pb-28 max-h-screen overflow-auto scrollbar-none flex-grow">
+      <div className="w-[70%] grid grid-cols-3 gap-4 content-evenly max-h-screen overflow-auto scrollbar-none flex-grow">
         {recipes.map((recipe) => (
-          <div key={recipe.id}
-               className="bg-white border border-gray-200 rounded-lg shadow-sm dark:bg-gray-800 dark:border-gray-300">
+          <div key={recipe.id} className="bg-white border border-gray-200 rounded-lg shadow-sm dark:bg-gray-800 dark:border-gray-300">
             <a className='' href="#">
               <Image width={500} height={500} src={recipe.picture} alt={"pizza"} className='rounded-t-lg'/>
             </a>
 
             <div className="p-5">
               <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{recipe.title}</h5>
+              <p className='text-lg tracking-tight text-gray-900 dark:text-white'>{recipe.time}</p>
               <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">{recipe.description}</p>
               <a href="#"
                  className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
