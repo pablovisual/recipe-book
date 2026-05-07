41dec5b5bdbc15fb7380589a8c88e8e611231d3c
pabarmsan <pabloarmandosandoval@gmail.com>
2026-05-07 18:58:55 -0500

    Add vercel live website url into CorsConfig file

--- recipe_book_DB/src/main/java/server/config/CorsConfig.java
+++ recipe_book_DB/src/main/java/server/config/CorsConfig.java
@@ -10,7 +10,7 @@ public class CorsConfig implements WebMvcConfigurer {
   @Override
   public void addCorsMappings(CorsRegistry registry) {
     registry.addMapping("/**")
-      .allowedOrigins("http://localhost:3000", "https://domain2.com")
+      .allowedOrigins("http://localhost:3000", "https://recipe-book-cyan-three.vercel.app")
       .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
       .allowedHeaders("*")
       .allowCredentials(true);
